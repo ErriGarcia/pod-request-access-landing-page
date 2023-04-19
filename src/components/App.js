8e5a55d9e0a71d9f7008b4df5f127de408c8cbfc
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-19 09:15:14 +0200

    add logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import '../styles/App.scss';
+import logo from "../images/desktop/logo.svg"
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo!</h1>
+      <img src={logo}></img>
+      <h1>Publish your podcasts everywhere.</h1>
+      <p>
+        Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
+        Apple Podcasts, Google Podcasts, Pocket Casts and more!
+        Request access
+      </p>
     </div>
   );
 }
