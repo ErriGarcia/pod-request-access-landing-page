3dbbaee1aca2a7671a6fbd81494657533d62c825
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-20 11:33:40 +0200

    add classes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,8 @@
 const Main = () => {
     return (
-        <main>
-            <h1>Publish your podcasts everywhere.</h1>
-            <p>
+        <main className='main'>
+            <h1 className='main-title'>Publish your podcasts everywhere.</h1>
+            <p className='main-description'>
                 Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
                 Apple Podcasts, Google Podcasts, Pocket Casts and more!
                 Request access
