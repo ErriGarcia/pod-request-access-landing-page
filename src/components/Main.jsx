83795c97cbe2ad88371369e5976bbd146febe4aa
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-20 12:08:41 +0200

    add style to description

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,9 @@
+import '../styles/Main.scss'
+
 const Main = () => {
     return (
         <main className='main'>
-            <h1 className='main-title'>Publish your podcasts everywhere.</h1>
+            <h1 className='main-title'>Publish your podcasts <span className='main-title-span'>everywhere.</span></h1>
             <p className='main-description'>
                 Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
                 Apple Podcasts, Google Podcasts, Pocket Casts and more!
